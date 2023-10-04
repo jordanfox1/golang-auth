b8955407800d3c8c33d8a1b8b473b284018e1b7d
Jordan Fox <jordan.fox@canstar.com.au>
2023-10-04 20:02:31 +1000

    initialised project

---
+++ routes/auth.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+	"auth/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(r *gin.Engine) {
+	r.POST("/login", controllers.Login)
+	r.POST("/signup", controllers.SignUp)
+	r.GET("/home", controllers.Home)
+	r.GET("/premium", controllers.Premium)
+	r.GET("/logout", controllers.Logout)
+}
